e1a916fc3118c7b19fd9d02f5125274ceca09464
LeulAria <leulsegedtariku@gmail.com>
2026-03-29 17:51:57 +0400

    fix: fix on build issue

---
+++ apps/desktop/src/renderer/src/voice-widget/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div style={{ color: 'white' }}>Voice Widget Placeholder</div>
+  </React.StrictMode>
+)
